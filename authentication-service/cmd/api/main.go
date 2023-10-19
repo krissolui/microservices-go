858b95ec65f3c8fbd101c193ef972712efbf989b
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-19 16:18:58 -0400

    added tests and refactored existing code for testing

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -19,8 +19,8 @@ const webPort = "80"
 var counts int64
 
 type Config struct {
-	DB     *sql.DB
-	Models data.Models
+	Repo   data.Repository
+	Client *http.Client
 }
 
 func main() {
@@ -34,9 +34,9 @@ func main() {
 
 	// set up config
 	app := Config{
-		DB:     conn,
-		Models: data.New(conn),
+		Client: &http.Client{},
 	}
+	app.setupRepo(conn)
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -84,4 +84,9 @@ func connectToDB() *sql.DB {
 		log.Println("Backing off for 2 sec...")
 		time.Sleep(2 * time.Second)
 	}
+}
+
+func (app *Config) setupRepo(conn *sql.DB) {
+	db := data.NewPostgresRepository((conn))
+	app.Repo = db
 }
